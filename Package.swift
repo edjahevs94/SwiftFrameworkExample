dbb557027077ef533385a8869afb9d9b346aa4e2
Edgard <20133252@ealoe.ulima.edu.pe>
2022-11-23 17:57:02 -0500

    Added changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "MyLibraryLearningExample",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyLibraryLearningExample",
+            type: .dynamic,
             targets: ["MyLibraryLearningExample"]),
     ],
     dependencies: [
