051969180674116c8ba2e585f8193ffc43094a60
Edgard <20133252@ealoe.ulima.edu.pe>
2022-11-24 08:19:17 -0500

    added new changes to Package.swift and test class

--- Tests/MyLibraryLearningExampleTests/MyLibraryLearningExampleTests.swift
+++ Tests/MyLibraryLearningExampleTests/MyLibraryLearningExampleTests.swift
@@ -1,11 +1,15 @@
 import XCTest
+import SwiftUI
 @testable import MyLibraryLearningExample
 
 final class MyLibraryLearningExampleTests: XCTestCase {
+  
+    
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MyLibraryLearningExample().text, "Hello, World!")
+        @State var text: String
+        
+        var body: some View {
+            MyLibraryLearningExample.MainTextField(placeholder: "Testing", text: $text)
+        }
     }
 }
